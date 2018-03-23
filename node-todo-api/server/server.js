195705fdb398fe6f3b800763a3e0ae94afaefeae
Berglind Lilja Björnsdóttir <jarvis@Mathieus-MacBook-Pro.local>
2018-03-23 13:14:17 +0000

    Lecture 72 completed

---
+++ node-todo-api/server/server.js
@@ -0,0 +1,65 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+
+});
+
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved Todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+//
+// });
+
+// var otherTodo = new Todo({
+//   text: 'Something to do'
+// });
+//
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save', e);
+// });
+
+//Create a user model
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+
+  }
+});
+
+  var newUser = new User({
+    email: 'andre@gmail.com'
+  });
+
+newUser.save().then((doc) => {
+  console.log('User saved', doc);
+}, (e) => {
+  console.log('Unable to save user', e);
+});
