2fbeae17015b7375ca6f8f180c3a5d0a3300d4ba
Berglind Lilja Björnsdóttir <jarvis@Mathieus-MacBook-Pro.local>
2018-04-22 19:31:00 +0000

    Added authentication middleware

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -53,6 +53,25 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+      decoded = jwt.verify(token, 'qbc123');
+  } catch (e) {
+    
+    return Promise.reject();
+
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
