08b2ab6e6649cc407b1f6887e9a5d539b7e73542
Berglind Lilja Björnsdóttir <jarvis@Mathieus-MacBook-Pro.local>
2018-04-22 18:52:24 +0000

    Add POST /users route and token method

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,25 +1,37 @@
 const {SHA256} = require('crypto-js');
-
-var message = "I am user number 3";
-var hash = SHA256(message).toString();
-
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+const jwt = require('jsonwebtoken');
 
 var data = {
-  id: 4
+  id: 10
 };
 
-var token = {
+var token = jwt.sign(data, '123qbc');
 
-  data,
-  hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-}
+var decoded = jwt.verify(token, '123qbc');
 
-var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// var message = "I am user number 3";
+// var hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
 
-if(resultHash === token.hash) {
-  console.log('Data was not changed');
-} else {
-  console.log('data was changed');
-}
+// var data = {
+//   id: 4
+// };
+//
+// var token = {
+//
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
+//
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+//
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+//
+// if(resultHash === token.hash) {
+//   console.log('Data was not changed');
+// } else {
+//   console.log('data was changed');
+// }
