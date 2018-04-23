369da69e17c70644b16f6262047d73e7a4176a32
Berglind Lilja Björnsdóttir <jarvis@Mathieus-MacBook-Pro.local>
2018-04-23 09:26:43 +0000

    Hash user password before saving

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,13 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123qbc');
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-var decoded = jwt.verify(token, '123qbc');
+var hashedPassword = '$2a$10$fZFkiNdC4Z8K9kumMCHdS.7dwVtupdOi9/zBdXUu1qoJL2yapc3PO';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123qbc');
+//
+// var decoded = jwt.verify(token, '123qbc');
 
 // var message = "I am user number 3";
 // var hash = SHA256(message).toString();
